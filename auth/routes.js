e948ed6ecf4186484ad30de26b409fd7b1241a94
Jesse Tomchak <jtomchak@gmail.com>
2016-09-27 18:46:45 -0700

    getting JWT !!!!!! amazing!!!!!!!!

--- auth/routes.js
+++ auth/routes.js
@@ -4,6 +4,6 @@ var verifyUser = require('./auth').verifyUser;
 // before we send back a jwt, lets check
 // the password and username match what is in the DB
 // this will be in by adding in the middleware
-router.post('/signin',verifyUser, controller.signin);
+router.post('/signin',verifyUser(), controller.signin);
 
 module.exports = router;
